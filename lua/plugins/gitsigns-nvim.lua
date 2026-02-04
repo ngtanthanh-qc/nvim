8c6be5840509c46530d3b642f6cf0697955b5494
ThanhMacOS <kaizen.seiko@outlook.com>
2026-02-05 06:15:29 +0700

    init dotfiles

---
+++ lua/plugins/gitsigns-nvim.lua
@@ -0,0 +1,14 @@
+-- ================================================================================================
+-- TITLE : gitsigns.nvim
+-- LINKS :
+--   > github : https://github.com/lewis6991/gitsigns.nvim
+-- ABOUT : deep buffer integration for git.
+-- ================================================================================================
+
+return {
+	"lewis6991/gitsigns.nvim",
+	opts = {},
+}
+
+
+
